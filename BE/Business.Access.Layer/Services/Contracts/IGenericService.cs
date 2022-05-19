22dff3a77fd77b67cc17b64e032f3d50a5385bde
DESKTOP-4B35MGH\User <damjan.tadic@htecgroup.com>
2022-05-19 09:41:13 +0200

    Created IGenericService

---
+++ BE/Business.Access.Layer/Services/Contracts/IGenericService.cs
@@ -0,0 +1,24 @@
+﻿using Business.Access.Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Access.Layer.Services.Contracts
+{
+    public interface IGenericService<TemplateEntity> where TemplateEntity : class, IBusinessEntityModel
+    {
+        Task<Guid?> Create(TemplateEntity model);
+
+        Task<TemplateEntity> GetById(Guid id);
+
+        Task<IEnumerable<TemplateEntity>> GetAll();
+
+        Task<TemplateEntity> GetByName(string name);
+
+        Task<TemplateEntity> Update(Guid id, TemplateEntity model);
+
+        Task<bool> Delete(Guid id);
+    }
+}
---
+++ BE/Business.Access.Layer/Services/GenericService.cs
@@ -0,0 +1,43 @@
+﻿using Business.Access.Layer.Models;
+using Business.Access.Layer.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Access.Layer.Services
+{
+    public class GenericService<TemplateEntity> : IGenericService<TemplateEntity> where TemplateEntity : class, IBusinessEntityModel
+    {
+        public Task<Guid?> Create(TemplateEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TemplateEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TemplateEntity> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TemplateEntity> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TemplateEntity> Update(Guid id, TemplateEntity model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
