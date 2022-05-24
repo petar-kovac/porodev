c14c559091c395d2aa16e931adb2c6d23312a08e
Damjan Tadic <damjan.tadic@htecgroup.com>
2022-05-24 09:30:05 +0200

    moved helper to api

---
+++ BE/Api.Access.Layer/Helpers/GlobalExceptionHandler/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using Business.Access.Layer.Helpers.GlobalExceptionHandler;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Service.Access.Layer.Helpers.GlobalExceptionHandler
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception error)
+            {
+                var response = httpContext.Response;
+                response.ContentType = "application/json";
+
+                string HRMessage;
+                switch (error)
+                {
+                    case Business.Access.Layer.Helpers.GlobalExceptionHandler.AppException e:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        HRMessage = e.HumanReadableErrorMessage;
+                        ExceptionLogger.WriteNewLog(HRMessage, e);
+                        break;
+                    case KeyNotFoundException e:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        HRMessage = "Key not found exception";
+                        ExceptionLogger.WriteNewLog(HRMessage, e);
+                        break;
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        HRMessage = "Internal server error";
+                        ExceptionLogger.WriteNewLog(HRMessage, new Exception());
+                        break;
+                }
+
+                var result = JsonSerializer.Serialize(new { humanReadableMessage = HRMessage, exceptionMessage = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class GlobalExceptionHandlerExtensions
+    {
+        public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+        }
+    }
+}
