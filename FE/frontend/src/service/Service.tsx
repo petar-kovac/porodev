049570baa0086b0cb1141326c79c5fa014b34001
Cedomir Sobic <cedomir.sobic@htecgroup.com>
2022-05-26 09:10:00 +0200

    Add project structure part one.

---
+++ FE/frontend/src/service/Service.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function Service() {
+  return <div>Service</div>;
+}
+
+export default Service;
---
+++ FE/frontend/src/util/Util.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function Util() {
+  return <div>Util</div>;
+}
+
+export default Util;
