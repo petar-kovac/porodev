5a2d0b01184696b4e2f9ef1d06e4ccac3bf9a84c
PoroQa <marko.ilic@htecgroup.com>
2022-05-31 13:00:25 +0200

    Added Driver setup for UI and Element control wrapper methods

--- QA/PoroDevTest/src/main/java/common/ui_setup/DriverSetup.java
+++ QA/PoroDevTest/src/main/java/common/ui_setup/DriverSetup.java
@@ -1,4 +1,60 @@
 package common.ui_setup;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.time.Duration;
+
 public class DriverSetup {
+    private WebDriver driver;
+    private int pageLoadTime;
+    private int elementDetectionTimeout;
+
+    public DriverSetup(DriverType browserType) {
+        pageLoadTime = 10;
+        elementDetectionTimeout = 10;
+
+        switch(browserType) {
+            case CHROME -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case FIREFOX -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            case EDGE -> {
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+    }
+
+    public void navigateToUrl(String url) {
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadTime));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(elementDetectionTimeout));
+        driver.get(url);
+    }
+    public WebDriver getDriver() {
+        return driver;
+    }
+    public void setDriver(WebDriver driver) {
+        this.driver = driver;
+    }
+    public int getPageLoadTime() {
+        return pageLoadTime;
+    }
+    public void setPageLoadTime(int pageLoadTime) {
+        this.pageLoadTime = pageLoadTime;
+    }
+    public int getElementDetectionTimeout() {
+        return elementDetectionTimeout;
+    }
+    public void setElementDetectionTimeout(int elementDetectionTimeout) {
+        this.elementDetectionTimeout = elementDetectionTimeout;
+    }
 }
