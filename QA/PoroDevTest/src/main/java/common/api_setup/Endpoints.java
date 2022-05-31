13275fb8cde325c487adda67d097d62c33e08c31
Jadranko Jankovic <jadranko.jankovic@htecgroup.com>
2022-05-31 08:43:28 +0200

    Adding ApiConfig class and Endpoints, updated gitignore

--- QA/PoroDevTest/src/main/java/common/api_setup/Endpoints.java
+++ QA/PoroDevTest/src/main/java/common/api_setup/Endpoints.java
@@ -1,4 +1,12 @@
 package common.api_setup;
 
 public class Endpoints {
+    public static String CREATE_USER = "User/create";
+    public static String UPDATE_USER = "User/Update";
+    public static String REGISTER_USER = "User/register/User";
+    public static String REGISTER_ADMIN = "User/register/admin";
+    public static String GET_USER_BY_EMAIL = "User/GetUserByEmail{email}";
+    public static String USER_LOGIN = "User/login";
+    public static String USER_DELETE = "User/delete";
+
 }
