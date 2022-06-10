8fe96f63d25c55f38cbd0c6a9a2dfe585c6bca13
Damjan Tadic <damjan.tadic@htecgroup.com>
2022-06-10 10:50:38 +0200

    added mapper to database

--- BE/PoroDev.Database/Consumers/UserCreateConsumer.cs
+++ BE/PoroDev.Database/Consumers/UserCreateConsumer.cs
@@ -1,4 +1,5 @@
-﻿using MassTransit;
+﻿using AutoMapper;
+using MassTransit;
 using PoroDev.Common.Contracts.Create;
 using PoroDev.Common.Models.UserModels.Data;
 using PoroDev.Database.Repositories.Contracts;
@@ -9,31 +10,19 @@ namespace PoroDev.Database.Consumers
     public class UserCreateConsumer : IConsumer<UserCreateRequestServiceToDatabase>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public UserCreateConsumer(IUnitOfWork unitOfWork)
+        public UserCreateConsumer(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task Consume(ConsumeContext<UserCreateRequestServiceToDatabase> context)
         {
-            var model = context.Message;
-
-            DataUserModel modelForDB = new DataUserModel()
-            {
-                Id = model.Id,
-                AvatarUrl = model.AvatarUrl,
-                Department = model.Department,
-                Email = model.Email,
-                Lastname = model.Lastname,
-                Name = model.Name,
-                Position = model.Position,
-                Role = model.Role,
-                Password = model.Password,
-                Salt = model.Salt,
-                DateCreated = DateTime.Now
-            };
+            var modelForDB = _mapper.Map<DataUserModel>(context.Message);
             var createdModel = await _unitOfWork.Users.CreateAsync(modelForDB);
+
             await _unitOfWork.SaveChanges();
 
             UserCreateResponseDatabaseToService returnModel = new()
@@ -42,10 +31,6 @@ namespace PoroDev.Database.Consumers
                 ExceptionName = null,
                 HumanReadableMessage = null
             };
-            returnmodel = UserCreate<UserCreateResponseDatabaseToService, DataUserModel>(createdModel);
-            returnmodel = UserCreate<UserCreateResponseDatabaseToService, DataUserModel>(exceprtion );
-
-
 
             await context.RespondAsync<UserCreateResponseDatabaseToService>(returnModel);
         }
---
+++ BE/PoroDev.Database/MapperProfiles/MapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PoroDev.Common.Contracts.Create;
+using PoroDev.Common.Models.UserModels.Data;
+
+namespace PoroDev.Database.MapperProfiles
+{
+    public class MapperProfiles : Profile
+    {
+        public MapperProfiles()
+        {
+            CreateMap<UserCreateRequestServiceToDatabase, DataUserModel>();
+        }
+    }
+}
