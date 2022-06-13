e9320fafe314c762fb877e09b7dac70f4e53eae2
DESKTOP-3G62TRP\User <daniela.strbac@htecgroup.com>
2022-06-13 17:07:47 +0200

    Added mapper to read user

--- BE/PoroDev.Database/Consumers/UserReadByEmailConsumer.cs
+++ BE/PoroDev.Database/Consumers/UserReadByEmailConsumer.cs
@@ -1,54 +1,60 @@
-﻿using MassTransit;
+﻿using AutoMapper;
+using MassTransit;
 using PoroDev.Common.Contracts.ReadUser;
-using PoroDev.Common.Exceptions;
-using PoroDev.Common.Models.UserModels.Data;
 using PoroDev.Database.Repositories.Contracts;
-using static PoroDev.Common.Extensions.CreateResponseExtension;
 
 namespace PoroDev.Database.Consumers
 {
     public class UserReadByEmailConsumer : IConsumer<UserReadByEmailRequestServiceToDatabase>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public UserReadByEmailConsumer(IUnitOfWork unitOfWork)
+        public UserReadByEmailConsumer(IUnitOfWork unitOfWork, IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task Consume(ConsumeContext<UserReadByEmailRequestServiceToDatabase> context)
         {
-            try
-            {
-                var fetchUser = await _unitOfWork.Users.FindAsync(user => user.Email.Equals(context.Message.Email.Trim()));
+            var fetchUser = await _unitOfWork.Users.FindAsync(user => user.Email.Equals(context.Message.Email.Trim()));
 
-                if (fetchUser == null)
-                {
-                    throw new UserNotFoundException("User with this email does not exist.");
-                }
+            var returnUser = _mapper.Map<UserReadByEmailResponseDatabaseToService>(fetchUser);
 
-                var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(fetchUser.Entity);
+            await context.RespondAsync(returnUser);
 
-                await context.RespondAsync(returnUser);
-            }
-            catch (UserNotFoundException ex)
-            {
-                string exceptionName = nameof(UserNotFoundException);
-                string humanReadableMessage = ex.HumanReadableErrorMessage;
+            //try
+            //{
+            //    var fetchUser = await _unitOfWork.Users.FindAsync(user => user.Email.Equals(context.Message.Email.Trim()));
 
-                var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(exceptionName, humanReadableMessage);
+            //    if (fetchUser == null)
+            //    {
+            //        throw new UserNotFoundException("User with this email does not exist.");
+            //    }
 
-                await context.RespondAsync(returnUser);
-            }
-            catch (Exception)
-            {
-                string exceptionName = nameof(DatabaseException);
-                string humanReadableMessage = "Internal database error.";
+            //    var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(fetchUser.Entity);
 
-                var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(exceptionName, humanReadableMessage);
+            //    await context.RespondAsync(returnUser);
+            //}
+            //catch (UserNotFoundException ex)
+            //{
+            //    string exceptionName = nameof(UserNotFoundException);
+            //    string humanReadableMessage = ex.HumanReadableErrorMessage;
 
-                await context.RespondAsync(returnUser);
-            }
+            //    var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(exceptionName, humanReadableMessage);
+
+            //    await context.RespondAsync(returnUser);
+            //}
+            //catch (Exception)
+            //{
+            //    string exceptionName = nameof(DatabaseException);
+            //    string humanReadableMessage = "Internal database error.";
+
+            //    var returnUser = CreateResponseModel<UserReadByEmailResponseDatabaseToService, DataUserModel>(exceptionName, humanReadableMessage);
+
+            //    await context.RespondAsync(returnUser);
+            //}
         }
     }
 }
--- BE/PoroDev.Database/MapperProfiles/MapperProfiles.cs
+++ BE/PoroDev.Database/MapperProfiles/MapperProfiles.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using PoroDev.Common.Contracts.Create;
+using PoroDev.Common.Contracts.ReadUser;
+using PoroDev.Common.Models.UnitOfWorkResponse;
 using PoroDev.Common.Models.UserModels.Data;
 
 namespace PoroDev.Database.MapperProfiles
@@ -9,6 +11,7 @@ namespace PoroDev.Database.MapperProfiles
         public MapperProfiles()
         {
             CreateMap<UserCreateRequestServiceToDatabase, DataUserModel>();
+            CreateMap<UnitOfWorkResponseModel<DataUserModel>, UserReadByEmailResponseDatabaseToService>();
         }
     }
 }
