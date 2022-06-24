ca459cc1970cbc89992aaa4a11ddeb7066404516
Damjan Tadic <damjan.tadic@htecgroup.com>
2022-06-24 11:16:02 +0200

    implemented read user by id with runtime data

---
+++ BE/PoroDev.DatabaseService/Consumers/UserConsumers/UserReadyByIdWithRuntimeData.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MassTransit;
+using PoroDev.Common.Contracts;
+using PoroDev.Common.Contracts.UserManagement.ReadByIdWithRuntime;
+using PoroDev.Common.Models.UserModels.Data;
+using PoroDev.DatabaseService.Repositories.Contracts;
+
+namespace PoroDev.DatabaseService.Consumers.UserConsumers
+{
+    public class UserReadyByIdWithRuntimeData : BaseDbConsumer, IConsumer<UserReadByIdWithRuntimeRequestServiceToDataBase>
+    {
+        public UserReadyByIdWithRuntimeData(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+        {
+        }
+
+        public async Task Consume(ConsumeContext<UserReadByIdWithRuntimeRequestServiceToDataBase> context)
+        {
+            var fetchUser = await _unitOfWork.Users.GetUserByIdWithRuntimeDatas(context.Message.Id);
+
+            var returnUser = _mapper.Map<CommunicationModel<DataUserModel>>(fetchUser);
+
+            await context.RespondAsync(returnUser);
+        }
+    }
+}
---
+++ BE/PoroDev.UserManagementService/Consumers/UserReadByIdWithRuntimeConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using PoroDev.Common.Contracts;
+using PoroDev.Common.Contracts.UserManagement.ReadByIdWithRuntime;
+using PoroDev.Common.Models.UserModels.Data;
+using PoroDev.UserManagementService.Services.Contracts;
+
+namespace PoroDev.UserManagementService.Consumers
+{
+    public class UserReadByIdConsumerWithRuntime : IConsumer<UserReadByIdWithRuntimeRequestGatewayToService>
+    {
+        private readonly IUserService _userService;
+
+        public UserReadByIdConsumerWithRuntime(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task Consume(ConsumeContext<UserReadByIdWithRuntimeRequestGatewayToService> context)
+        {
+            var returnuser = await _userService.ReadUserByIdWithRuntimeData(context.Message);
+
+            await context.RespondAsync(returnuser);
+        }
+    }
+}
