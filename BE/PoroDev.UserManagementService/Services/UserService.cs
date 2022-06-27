1877431a6e939c41d72d8324b3f2eef1dcd64986
bosko.gogic <bosko.gogic@htecgroup.com>
2022-06-27 09:35:41 +0200

    Fixed Update operation and changed Email_Lenght_Error const

--- BE/PoroDev.UserManagementService/Services/UserService.cs
+++ BE/PoroDev.UserManagementService/Services/UserService.cs
@@ -332,6 +332,13 @@ namespace PoroDev.UserManagementService.Services
 
         public async Task<CommunicationModel<DataUserModel>> UpdateUser(UserUpdateRequestGatewayToService model)
         {
+            var isException = await CheckUserFieldsUpdate(model);
+
+            if (isException.ExceptionName != null)
+            {
+                return isException;
+            }
+
             GetHashAndSalt(model.PasswordUnhashed, out byte[] salt, out byte[] hash);
 
             var updateUserRequest = _mapper.Map<UserUpdateRequestServiceToDatabase>(model);
@@ -363,6 +370,112 @@ namespace PoroDev.UserManagementService.Services
             return returnContext;
         }
 
-        
+        private async Task<CommunicationModel<DataUserModel>> CheckUserFieldsUpdate(UserUpdateRequestGatewayToService updateModel)
+        {
+            try
+            {
+                CheckFullName(updateModel.Name, updateModel.Lastname);
+                await CheckEmailUpdate(updateModel.Email);
+                CheckPassword(updateModel.PasswordUnhashed);
+                CheckPosition(updateModel.Position);
+            }
+            catch (EmailFormatException ex)
+            {
+
+                string exceptionType = nameof(EmailFormatException);
+                string humanReadableMessage = ex.HumanReadableErrorMessage;
+
+                var responseException = new CommunicationModel<DataUserModel>()
+                {
+                    Entity = null,
+                    ExceptionName = exceptionType,
+                    HumanReadableMessage = humanReadableMessage
+                };
+
+                return responseException;
+            }
+            catch (PasswordFormatException ex)
+            {
+                string exceptionType = nameof(PasswordFormatException);
+                string humanReadableMessage = ex.HumanReadableErrorMessage;
+
+                var responseException = new CommunicationModel<DataUserModel>()
+                {
+                    Entity = null,
+                    ExceptionName = exceptionType,
+                    HumanReadableMessage = humanReadableMessage
+                };
+
+                return responseException;
+            }
+            catch (FullNameFormatException ex)
+            {
+                string exceptionType = nameof(FullNameFormatException);
+                string humanReadableMessage = ex.HumanReadableErrorMessage;
+
+                var responseException = new CommunicationModel<DataUserModel>()
+                {
+                    Entity = null,
+                    ExceptionName = exceptionType,
+                    HumanReadableMessage = humanReadableMessage
+                };
+
+                return responseException;
+            }
+            catch (PositionFormatException ex)
+            {
+                string exceptionType = nameof(PositionFormatException);
+                string humanReadableMessage = ex.HumanReadableErrorMessage;
+
+                var responseException = new CommunicationModel<DataUserModel>()
+                {
+                    Entity = null,
+                    ExceptionName = exceptionType,
+                    HumanReadableMessage = humanReadableMessage
+                };
+
+                return responseException;
+            }
+
+            return new CommunicationModel<DataUserModel>() { Entity = null, ExceptionName = null, HumanReadableMessage = null };
+        }
+
+        private async Task CheckEmailUpdate(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                throw new EmailFormatException(EMAIL_EMPTY_ERROR);
+            }
+
+            if (email.Length > MAX_EMAIL_LENGTH)
+            {
+                throw new EmailFormatException(EMAIL_LENGTH_ERROR);
+            }
+
+            var splitEmail = email.Split('@');
+
+            if (splitEmail.Length != 2 || string.IsNullOrWhiteSpace(splitEmail[0]))
+            {
+                throw new EmailFormatException(EMAIL_FORMAT_ERROR);
+            }
+
+            if (splitEmail[0].Any(x => char.IsWhiteSpace(x)))
+            {
+                throw new EmailFormatException(EMAIL_WHITESPACE_ERROR);
+            }
+
+            if (CheckStringForCharacters(splitEmail[0], SPECIAL_CHARACTERS_EMAIL_STRING))
+            {
+                throw new EmailFormatException(EMAIL_SPECIAL_CHARACTERS_ERROR);
+            }
+
+            if (!splitEmail[1].Equals(EMAIL_DOMAIN))
+            {
+                throw new EmailFormatException(EMAIL_DOMAIN_ERROR);
+            }
+
+        }
+
+
     }
 }
