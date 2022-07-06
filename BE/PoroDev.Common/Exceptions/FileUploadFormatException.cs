ad5a663619c359f1e27886d164d919dc991286e0
DESKTOP-3G62TRP\User <daniela.strbac@htecgroup.com>
2022-07-06 16:00:31 +0200

    Custom exception updated

--- BE/PoroDev.Common/Exceptions/FileUploadFormatException.cs
+++ BE/PoroDev.Common/Exceptions/FileUploadFormatException.cs
@@ -1,8 +1,9 @@
-﻿using System.Globalization;
+﻿using PoroDev.Common.Exceptions.Contract;
+using System.Globalization;
 
 namespace PoroDev.Common.Exceptions
 {
-    public class FileUploadFormatException : Exception
+    public class FileUploadFormatException : Exception, ICustomException
     {
         public string HumanReadableErrorMessage { get; set; }
 
