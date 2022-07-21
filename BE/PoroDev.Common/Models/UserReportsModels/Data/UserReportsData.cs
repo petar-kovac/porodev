79b0f8c3bc3287b2c7f65fa3d1fb72152d20a8bd
bosko.gogic <bosko.gogic@htecgroup.com>
2022-07-21 11:29:37 +0200

    Fixed some things in UserReportsData.

--- BE/PoroDev.Common/Models/UserReportsModels/Data/UserReportsData.cs
+++ BE/PoroDev.Common/Models/UserReportsModels/Data/UserReportsData.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using PoroDev.Common.Models.UserModels.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PoroDev.Common.Models.UserReportsModels.Data
@@ -16,17 +18,23 @@ namespace PoroDev.Common.Models.UserReportsModels.Data
 
         public string Month { get; set; }
 
+        public Guid CurrentUserId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.Always)]
+        public DataUserModel CurrentUser { get; set; }
+
         public UserReportsData()
         {
 
         }
 
-        public UserReportsData(ulong fileDownloadTotal, ulong fileUploadTotal, ushort runtimeTotal, string month)
+        public UserReportsData(ulong fileDownloadTotal, ulong fileUploadTotal, ushort runtimeTotal, string month, Guid currentUserId)
         {
             FileDownloadTotal = fileDownloadTotal;
             FileUploadTotal = fileUploadTotal;
             RuntimeTotal = runtimeTotal;
             Month = month;
+            CurrentUserId = currentUserId;
         }
     }
 }
