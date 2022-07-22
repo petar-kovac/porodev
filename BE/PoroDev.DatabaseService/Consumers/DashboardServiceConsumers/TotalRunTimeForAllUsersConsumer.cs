ac50be2828b63089b820c0694b60e37aa8fcfb9e
bosko.gogic <bosko.gogic@htecgroup.com>
2022-07-22 16:44:52 +0200

    Deleted comments inside TotalRunTimeForAllUsersConsumer class.

--- BE/PoroDev.DatabaseService/Consumers/DashboardServiceConsumers/TotalRunTimeForAllUsersConsumer.cs
+++ BE/PoroDev.DatabaseService/Consumers/DashboardServiceConsumers/TotalRunTimeForAllUsersConsumer.cs
@@ -22,37 +22,6 @@ namespace PoroDev.DatabaseService.Consumers.DashboardServiceConsumers
 
         public async Task Consume(ConsumeContext<TotalRunTimeForAllUsersRequestServiceToDatabase> context)
         {
-            //DataUserModel user = await _unitOfWork.Users.GetByIdAsync(context.Message.UserId);
-
-            //if (user.Role == 0)
-            //{
-            //    TotalRunTimeForAllUsersModel returnModel = new TotalRunTimeForAllUsersModel();
-            //    returnModel.NumberOfTotalRunTimeForAllUsers = await CountNumberOfRunTimeExecutions();
-
-            //    var response = new CommunicationModel<TotalRunTimeForAllUsersModel>()
-            //    {
-            //        Entity = returnModel,
-            //        ExceptionName = null,
-            //        HumanReadableMessage = null
-            //    };
-
-            //    await context.RespondAsync<CommunicationModel<TotalRunTimeForAllUsersModel>>(response);
-            //}
-            //else
-            //{
-            //    string exceptionType = nameof(UserIsNotAdminException);
-            //    string humanReadableMessage = "User must be admin!";
-
-            //    var resposneException = new CommunicationModel<TotalRunTimeForAllUsersModel>()
-            //    {
-            //        Entity = null,
-            //        ExceptionName = exceptionType,
-            //        HumanReadableMessage = humanReadableMessage
-            //    };
-
-            //    await context.RespondAsync(resposneException);
-            //}
-
             var responseModel = await TotalRunTimeForAllUsers(context.Message);
 
             await context.RespondAsync<CommunicationModel<TotalRunTimeForAllUsersModel>>(responseModel);
@@ -82,14 +51,5 @@ namespace PoroDev.DatabaseService.Consumers.DashboardServiceConsumers
             return responseTotalMemoryUsedForUploadPerMonth;
         }
 
-        private async Task<int> CountNumberOfRunTimeExecutions()
-        {
-            List<RuntimeData> runTimeDataFiles = (await _unitOfWork.RuntimeData.FindAllAsync(userRuntime => userRuntime.User.Email.Contains(""))).ToList<RuntimeData>();
-
-            int countTotalRunTimeForAllUsers = 0;
-            countTotalRunTimeForAllUsers = runTimeDataFiles.Count;
-
-            return countTotalRunTimeForAllUsers;
-        }
     }
 }
