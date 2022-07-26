b56ff4f7529f85cefbfb5f74630a1a2c3a3c1d4d
Damjan Tadic <damjan.tadic@htecgroup.com>
2022-07-26 11:10:38 +0200

    roll back changes in user service

--- BE/PoroDev.UserManagementService/Services/UserService.cs
+++ BE/PoroDev.UserManagementService/Services/UserService.cs
@@ -255,25 +255,24 @@ namespace PoroDev.UserManagementService.Services
 
             userToRegister.Salt = salt;
             userToRegister.Password = hash;
-            //userToRegister.VerificationToken = CreateRandomVerificationToken();
-            userToRegister.VerificationToken = "ddddd";
+            userToRegister.VerificationToken = CreateRandomVerificationToken();
 
-            //userToRegister.FileUploadTotal = 0;
-            //userToRegister.FileDownloadTotal = 0;
-            //userToRegister.RuntimeTotal = 0;
+            userToRegister.FileUploadTotal = 0;
+            userToRegister.FileDownloadTotal = 0;
+            userToRegister.RuntimeTotal = 0;
 
             var requestResponseContext = await _registerUserClient.GetResponse<CommunicationModel<DataUserModel>>(userToRegister, CancellationToken.None, RequestTimeout.After(m: 5));
 
-            //var otherProperties = new Dictionary<string, string>() {{"VerificationToken", userToRegister.VerificationToken }};
-            //var emailProperties = CreateVerificationEmailProperties(userToRegister.Email, otherProperties);
+            var otherProperties = new Dictionary<string, string>() { { "VerificationToken", userToRegister.VerificationToken } };
+            var emailProperties = CreateVerificationEmailProperties(userToRegister.Email, otherProperties);
 
-            //var verificationEmailResponseContext = await _verificationEmailSenderRequestClient.GetResponse<CommunicationModel<SendEmailModel>>(emailProperties);
+            var verificationEmailResponseContext = await _verificationEmailSenderRequestClient.GetResponse<CommunicationModel<SendEmailModel>>(emailProperties);
 
-            //if (verificationEmailResponseContext.Message.Entity == null || verificationEmailResponseContext.Message.Entity.StatusCode != HttpStatusCode.Accepted)
-            //{
-            //    await _deleteUserRequestClient.GetResponse<CommunicationModel<DeleteUserModel>>(new UserDeleteRequestServiceToDatabase() { Email = userToRegister.Email});
-            //    return CreateResponseModel<CommunicationModel<RegisterUserResponse>, RegisterUserResponse>(nameof(FailedToRegisterUserException), FailedToRegisterUserExceptionMessage);
-            //}
+            if (verificationEmailResponseContext.Message.Entity == null || verificationEmailResponseContext.Message.Entity.StatusCode != HttpStatusCode.Accepted)
+            {
+                await _deleteUserRequestClient.GetResponse<CommunicationModel<DeleteUserModel>>(new UserDeleteRequestServiceToDatabase() { Email = userToRegister.Email });
+                return CreateResponseModel<CommunicationModel<RegisterUserResponse>, RegisterUserResponse>(nameof(FailedToRegisterUserException), FailedToRegisterUserExceptionMessage);
+            }
             return _mapper.Map<CommunicationModel<RegisterUserResponse>>(requestResponseContext.Message);
         }
 
