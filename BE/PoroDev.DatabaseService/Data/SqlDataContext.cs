81779c66765ba6fbf85f18e8813d6d6ad5ee6b99
DESKTOP-KECVUV8\User <srdjanstanojcic031@gmail.com>
2022-07-27 11:20:19 +0200

    added feature to restart monthly limits on the start of new month and save current values to user reports data table

--- BE/PoroDev.DatabaseService/Data/SqlDataContext.cs
+++ BE/PoroDev.DatabaseService/Data/SqlDataContext.cs
@@ -4,6 +4,7 @@ using PoroDev.Common.Models.RuntimeModels.Data;
 using PoroDev.Common.Models.SharedSpaces;
 using PoroDev.Common.Models.StorageModels.Data;
 using PoroDev.Common.Models.UserModels.Data;
+using PoroDev.Common.Models.UserReportsModels.Data;
 
 namespace PoroDev.DatabaseService.Data
 {
@@ -20,6 +21,7 @@ namespace PoroDev.DatabaseService.Data
         public virtual DbSet<SharedSpacesUsers> SharedSpacesUsers { get; set; } = default!;
         public virtual DbSet<SharedSpacesFiles> SharedSpacesFiles { get; set; } = default!;
         public virtual DbSet<NotificationDataModel> NotificationData { get; set; } = default!;
+        public virtual DbSet<UserReportsData> UserReportsData { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ BE/PoroDev.StorageService/Services/MonthlyLimitRestartService.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using PoroDev.Common.Contracts;
+using PoroDev.Common.Contracts.StorageService.RestartMonthlyLimits;
+using PoroDev.Common.Models.StorageModels;
+using Sgbj.Cron;
+
+namespace PoroDev.StorageService.Services
+{
+    public class MonthlyLimitRestartService : BackgroundService
+    {
+        private readonly IRequestClient<RestartMonthlyLimitsRequestServiceToDatabase> _restartMonthlyLimitsRequestClient;
+        
+        public MonthlyLimitRestartService(IServiceScopeFactory factory)
+        {
+            _restartMonthlyLimitsRequestClient = factory.CreateScope().ServiceProvider.GetRequiredService<IRequestClient<RestartMonthlyLimitsRequestServiceToDatabase>>();
+        }
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var model = new RestartMonthlyLimitsRequestServiceToDatabase();
+            var _timer = new CronTimer("0 0 1 * *", TimeZoneInfo.Local);
+            while(await _timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested)
+            {
+                await _restartMonthlyLimitsRequestClient.GetResponse<CommunicationModel<RestartModel>>(model);
+            }
+        }
+    }
+}
